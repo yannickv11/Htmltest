b28550982606baf02a17e7278d9d95bc045c9e60
yannick <yannick.vermeijden@student.rocvf.nl>
2018-12-07 13:44:10 +0100

    javascript1 af

---
+++ javascript/mijnJs.js
@@ -0,0 +1,15 @@
+var a = 10;
+if (a > 9) {
+    console.log("hallo hij is groter ");
+}
+for (var i = 0; i <10 ; i++) {
+    console.log(" i heeft nu de waarde " + i );
+}
+
+ var mijnauto = {
+     merk: "audi",
+     type: "rs6",
+     AantalWielen: 4,
+     Kleur:"roze"
+ }
+ console.log(mijnauto.Kleur);
