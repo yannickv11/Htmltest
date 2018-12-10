db4c069071755e3a3e8a5bade55ece5e6d0273d7
yannick <yannick.vermeijden@student.rocvf.nl>
2018-12-10 11:39:22 +0100

    javascript2 af

---
+++ javascript/Javascript2/Script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("paginatitel").innerText = "Dit is de pagina titel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("bal1").src = "img/bal1.jpg";
+document.getElementById("bal2").src = "img/bal2.jpg";
---
+++ javascript/Javascript2/Script2.js
@@ -0,0 +1,4 @@
+document.getElementById("bl1").style.backgroundColor = "rgb(0, 255, 255)";
+document.getElementById("bl2").style.backgroundColor = "rgb(0, 102, 0)";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red"
